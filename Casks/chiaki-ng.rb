45a74baacde5c73fa0933ce2a3ad19504571b1a2
Street Pea <streetpea@proton.me>
2024-12-27 21:08:34 -0800

    Update amd64 version of chiaki-ng for 1.9.3 since previous one had a packaging issue

--- Casks/chiaki-ng.rb
+++ Casks/chiaki-ng.rb
@@ -3,7 +3,7 @@ cask "chiaki-ng" do
 
   version "1.9.3"
   sha256 arm:   "62e16bdda346ca30920972d195c60ae855a1589e09c0ba0e8c61d6d93174b40f",
-         intel: "bfa5d3922d9073ca8430d2f43d9011eb5146c42b9b167363f015565ba2b3698c"
+         intel: "a4d0056130d79c4a3bae6a424304e1abeefcd6709be1755c7da0b0415b0a8a24"
 
   url "https://github.com/streetpea/chiaki-ng/releases/download/v#{version}/chiaki-ng-macos_#{arch}-Release.dmg",
       verified: "github.com/streetpea/chiaki-ng/"
