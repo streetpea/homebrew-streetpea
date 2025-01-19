79575cfefe245faacce4fff0185eaad2a08d6d38
Street Pea <streetpea@proton.me>
2025-01-19 14:12:58 -0800

    Use updated amd64 dmg for chiaki-ng v1.9.5

--- Casks/chiaki-ng.rb
+++ Casks/chiaki-ng.rb
@@ -1,9 +1,9 @@
 cask "chiaki-ng" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.9.5,0"
+  version "1.9.5,1"
   sha256 arm:   "b22f646e26d78431029f3055def47c9cd4215c4babf4abbb6a243a8eb0d91543",
-         intel: "cea5e3f1fb0a5c28259394f51b87f5869897a66de215107f85e6c13f9daf7c60"
+         intel: "be8636508a679a5fad7ff0092181434d552df4a65b84a91280738b7e5accdc81"
 
   url "https://github.com/streetpea/chiaki-ng/releases/download/v#{version.csv.first}/chiaki-ng-macos_#{arch}-Release.dmg",
       verified: "github.com/streetpea/chiaki-ng/"
